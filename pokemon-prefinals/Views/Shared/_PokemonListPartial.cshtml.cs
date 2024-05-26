b4856c0871a9e1035da4425d5d6799c5409031ff
Byron Reyes <codclips0503@gmail.com>
2024-05-27 04:31:19 +0800

    First commit

---
+++ pokemon-prefinals/Views/Shared/_PokemonListPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace pokemon_prefinals.Views.Shared
+{
+    public class _PokemonListPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
